9d9a63dfedf8db47ee81c1e38854b12decb3014b
Seth Shaw <seth.shaw@unlv.edu>
2019-07-15 11:50:09 -0700

    avoid undefined error when no data provided

--- dist/jexcel.js
+++ dist/jexcel.js
@@ -233,7 +233,8 @@ var jexcel = (function(el, options) {
         // Number of columns
         var size = obj.options.columns.length;
 
-        if (obj.options.data[0].length > size) {
+        if (typeof obj.options.data[0] !== 'undefined' &&
+            obj.options.data[0].length > size) {
             size = obj.options.data[0].length;
         }
 
