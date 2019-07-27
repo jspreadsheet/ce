e132f8612310464ccd896fd9f34bd81d52b13c9c
rozwell <rozwell69@gmail.com>
2019-07-28 00:36:27 +0200

    Fixed adding columns to respect defaultColWidth option

--- dist/jexcel.js
+++ dist/jexcel.js
@@ -3351,7 +3351,7 @@ var jexcel = (function(el, options) {
             }
             for (var i = 0; i < numOfColumns; i++) {
                 if (! properties.columns[i]) {
-                    properties.columns[i] = { type:'text', source:[], options:[], width:'50', align:'center' };
+                    properties.columns[i] = { type:'text', source:[], options:[], width:obj.options.defaultColWidth, align:'center' };
                 }
             }
 
