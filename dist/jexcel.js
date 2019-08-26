16d2198bd96be42bc3450f217e1a215a8c353999
paulhodel <paulhodel@DESKTOP-HVAO2Q0>
2019-08-26 14:28:27 +0100

    onbeforechange optional interception

--- dist/jexcel.js
+++ dist/jexcel.js
@@ -1773,7 +1773,7 @@ var jexcel = (function(el, options) {
                     var val = obj.options.onbeforechange(el, obj.records[y][x], x, y, value);
 
                     // If you return something this will overwrite the value
-                    if (val != 'undefined') {
+                    if (val != undefined) {
                         value = val;
                     }
                 }
