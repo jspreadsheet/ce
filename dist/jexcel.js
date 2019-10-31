ed2ca41aa3802cda3866c969a487cc49a7e9b548
paulhodel <paulhodel@DESKTOP-HVAO2Q0>
2019-10-31 11:38:57 +0000

    Pre-release for v5.4

--- dist/jexcel.js
+++ dist/jexcel.js
@@ -1,5 +1,5 @@
 /**
- * (c) jExcel v3.5.2
+ * (c) jExcel v3.5.3
  * 
  * Author: Paul Hodel <paul.hodel@gmail.com>
  * Website: https://bossanova.uk/jexcel/
@@ -187,7 +187,7 @@ var jexcel = (function(el, options) {
             noCellsSelected: 'No cells selected',
         },
         // About message
-        about:"jExcel CE Spreadsheet\nVersion 3.5.2\nAuthor: Paul Hodel <paul.hodel@gmail.com>\nWebsite: https://bossanova.uk/jexcel/v3",
+        about:"jExcel CE Spreadsheet\nVersion 3.5.3\nAuthor: Paul Hodel <paul.hodel@gmail.com>\nWebsite: https://bossanova.uk/jexcel/v3",
     };
 
     // Loading initial configuration from user
